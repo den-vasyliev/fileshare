fb6a83fd6c62cf8020825722731c25e89634736b
Denys Vasyliev [GL] <Denys.Vasyliev@qsc.com>
2021-12-02 17:19:06 +0200

    add main

---
+++ src/main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+    "fmt"
+    "net/url"
+    "net/http"
+    "log"
+    "io/ioutil"
+    "os"
+    "strings"
+            "crypto/hmac"
+        "crypto/sha1"
+        "encoding/base64"
+        "time"
+       "strconv"
+)
+
+// sign URL with key and return sign
+func sign(url, keyName string, expiration time.Time, s string) ([]byte, error) {
+      
+        keyPath := os.Getenv("KEY_PATH")
+        // Note: consider using the GCP Secret Manager for managing access to your
+        // signing key(s).
+        key, err := readKeyFile(keyPath)
+        if err != nil {
+                log.Print(err)
+        return nil, fmt.Errorf("failed to base64url decode: %+v", err)
+        }
+
+        sep := "?"
+        if strings.Contains(url, "?") {
+                sep = "&"
+        }
+        url += sep
+        url += fmt.Sprintf("Expires=%d", expiration.Unix())
+        url += fmt.Sprintf("&KeyName=%s", keyName)
+
+        mac := hmac.New(sha1.New, key)
+        mac.Write([]byte(url))
+        sig := base64.URLEncoding.EncodeToString(mac.Sum(nil))
+        //url += fmt.Sprintf("&Signature=%s", sig)
+        
+
+        if s == sig {
+
+        return []byte(sig), nil
+        }else{
+        return nil, fmt.Errorf("failed to base64url decode: %+v", err)
+    }
+}
+
+
+
+// readKeyFile reads the base64url-encoded key file and decodes it.
+func readKeyFile(path string) ([]byte, error) {
+        b, err := ioutil.ReadFile(path)
+        if err != nil {
+                return nil, fmt.Errorf("failed to read key file: %+v", err)
+        }
+        d := make([]byte, base64.URLEncoding.DecodedLen(len(b)))
+        n, err := base64.URLEncoding.Decode(d, b)
+        if err != nil {
+                return nil, fmt.Errorf("failed to base64url decode: %+v", err)
+        }
+        return d[:n], nil
+}
+
+// start server
+type indexHandler struct {
+}
+
+func (h *indexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    m, err := url.ParseQuery(r.URL.RawQuery)
+    if err != nil {
+        http.Error(w, http.StatusText(403), 403)
+        return
+        }
+        
+        req, err := fmt.Printf("Req: %s %s %s\n", r.URL.Path, r.Host, r.URL.RawQuery )
+
+            // The path to a file containing the base64-encoded signing key
+        if err != nil {
+        log.Print(err)
+        log.Print("Checklist")
+        http.Error(w, http.StatusText(403), 403)
+        return
+        }else{
+            log.Print(req)
+        }
+    if len(m["Expires"]) == 0 {
+        log.Print("Expires")
+        http.Error(w, http.StatusText(403), 403)
+        return
+    }
+
+    ttl, err := strconv.Atoi(m["Expires"][0])
+    if err != nil {
+        log.Print("Atoi")
+        log.Print(err)
+        http.Error(w, http.StatusText(403), 403)
+        return
+    }
+    
+    scheme := "https://"
+    if r.URL.Scheme == "" {
+        scheme = "http://"
+    }
+    //fmt.Printf("%s%s%s", scheme, r.Host, r.URL.Path)
+    // validate and compare sign
+    //.Print(len(m["Signature"][0]))
+    if len(m["Signature"][0]) == 0 {
+
+        log.Print("Signature")
+
+        http.Error(w, http.StatusText(403), 403)
+    }else{
+
+    s, err := sign(fmt.Sprintf("%s%s%s", scheme, r.Host, r.URL.Path),m["KeyName"][0],time.Unix(int64(ttl), 0),m["Signature"][0])
+
+    if err != nil {
+        log.Print("Not Valid")
+        log.Print(string(s),m["Signature"][0],err)
+        http.Error(w, http.StatusText(403), 403)
+    }else{
+    log.Print(string(s),m["Signature"][0],err)
+    http.ServeFile(w, r, fmt.Sprintf(".%s",r.URL.Path))
+    }
+
+    }
+
+
+
+}
+
+// main
+func main() {
+    httpServer := &http.Server{Addr: fmt.Sprintf("%v:%v", "localhost", "8080"), Handler: &indexHandler{}}
+        err := httpServer.ListenAndServe()
+        if err != nil {
+            fmt.Println("Err from server")
+            fmt.Println(err)
+        }
+
+}
